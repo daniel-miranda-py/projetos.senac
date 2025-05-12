03822e72ddd5f3fe415b17652080b2dc2123d71e
daniel-miranda-py <mirandovski.studies@gmail.com>
2025-05-12 13:47:11 -0300

    modulo 01 projeto final

---
+++ projeto-final-modulo-01/projeto-completo/java-script/header.js
@@ -0,0 +1,8 @@
+
+
+        function navBar() {
+        const navButton = document.getElementsByClassName('nav-button-icon'); 
+        const navContainer = document.getElementById('nav-container');
+        navContainer.classList.toggle('ativo');
+        navButton.classList.toggle('ativo');
+}
